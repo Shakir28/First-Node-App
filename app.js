46cb9766661bd0195d630d7b895c65fe28e483a6
Shakir Ali <shakirali@Shakirs-MacBook-Pro.local>
2023-08-27 21:06:33 +0530

    res user data

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+const data = require('./data.json');
+console.log(`data :: ${JSON.stringify(data)}`)
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send(JSON.stringify(data))
 });
 
 app.listen(5000, () => console.log('App is running in port 5000'))
